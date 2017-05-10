cddec86361b94c3bc670fc852bb4f59a950745e9
Paul Dilyard <paul.dilyard@gmail.com>
2017-05-10 10:55:57 -0400

    docker-machine + dinghy setup

---
+++ configure-docker-machine.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
