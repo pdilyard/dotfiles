7ce84a5ba3a2ef99c77cb48ed80476256e9060a0
Paul Dilyard <paul.dilyard@gmail.com>
2017-08-28 18:54:17 -0400

    Fix gcam

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,9 +9,9 @@ abbr lock '/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resource
 abbr ga 'git add'
 abbr gaa 'git add -A'
 abbr gc 'git commit'
-abbr gca 'git commit --all'
-abbr gcam 'git commit --all -m '
-abbr gcm 'git commit -m '
+abbr gca 'git add -A; git commit'
+abbr gcam 'git add -A; git commit -m'
+abbr gcm 'git commit -m'
 abbr gco 'git checkout'
 abbr gd 'git diff'
 abbr glo "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
